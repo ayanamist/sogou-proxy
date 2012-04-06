d1f6885a45d5ad745a1c3e78788658af43fe5247
ayanamist <ayanamist@gmail.com>
2012-04-07 00:47:44 +0800

    refactor

--- proxy.py
+++ proxy.py
@@ -103,34 +103,6 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
                 logging.exception("")
 
     # CONNECT Data Transfer
-    def transfer(self, soc_r, soc_w):
-        fdset = [soc_r, soc_w]
-        data = ''
-        while True:
-            r, w, _ = select.select(fdset, fdset, [])
-            for soc in fdset:
-                try:
-                    i = r.index(soc)
-                except ValueError:
-                    pass
-                else:
-                    try:
-                        data += soc.recv(BUFFER_SIZE)
-                    except socket, e:
-                        if e.errno == errno.WSAECONNRESET:
-                            self.send_error(httplib.BAD_GATEWAY,
-                                'An existing connection was forcibly closed by the remote host')
-                        else:
-                            logging.exception(str(e))
-                    else:
-                        if not data:
-                            break
-                        the_other_soc = fdset[i ^ 1]
-                        if the_other_soc in w:
-                            the_other_soc.sendall(data)
-                            data = ''
-
-
     def remote_connect(self):
         self.remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.remote.settimeout(None)
@@ -167,7 +139,32 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
             self.remote.sendall(self.rfile.read(int(self.headers['Content-Length'])))
 
     def local_write_connect(self):
-        return self.transfer(self.remote, self.connection)
+        fdset = [self.remote, self.connection]
+        data = ''
+        while True:
+            r, w, _ = select.select(fdset, fdset, [])
+            if r:
+                for soc in fdset:
+                    try:
+                        i = r.index(soc)
+                    except ValueError:
+                        pass
+                    else:
+                        try:
+                            data += soc.recv(BUFFER_SIZE)
+                        except socket, e:
+                            if e.errno == errno.WSAECONNRESET:
+                                self.send_error(httplib.BAD_GATEWAY,
+                                    'An existing connection was forcibly closed by the remote host')
+                            else:
+                                logging.exception(str(e))
+                        else:
+                            if not data:
+                                break
+                            the_other_soc = fdset[i ^ 1]
+                            if the_other_soc in w:
+                                the_other_soc.sendall(data)
+                                data = ''
 
     def local_write_other(self):
         while True:
