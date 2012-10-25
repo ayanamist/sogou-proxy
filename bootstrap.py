031e5639c9f85a581140902d1cfbe5c66e8c4321
ayanamist <ayanamist@gmail.com>
2012-10-25 11:57:19 +0800

    Use any exist egg file.

--- bootstrap.py
+++ bootstrap.py
@@ -4,8 +4,11 @@ import sys
 sys.dont_write_bytecode = True
 
 executable_path = getattr(sys, 'executable', sys.argv[0])
-
-sys.path.append(os.path.dirname(executable_path))
+executable_dir = os.path.dirname(executable_path)
+sys.path.append(executable_dir)
+for filename in os.listdir(executable_dir):
+    if os.path.splitext(filename)[1].lower() == ".egg":
+        sys.path.append(os.path.join(executable_dir, filename))
 
 __file__ = os.path.abspath(os.path.splitext(executable_path)[0] +'.py')
 if os.path.exists(__file__):
