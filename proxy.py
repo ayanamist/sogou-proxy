c7e6d06b648b2464fbbe5f1182fa95428d319bb3
ayanamist <ayanamist@gmail.com>
2012-11-22 18:13:02 +0800

    absolute_import

--- proxy.py
+++ proxy.py
@@ -11,6 +11,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from __future__ import absolute_import
+
 __author__ = "ayanamist"
 __copyright__ = "Copyright 2012"
 __license__ = "GPL"
