6440cf36f5e727264f7d5cb73a5c829d74bad898
ayanamist <ayanamist@gmail.com>
2013-02-25 17:36:22 +0800

    Upgrade tornado_pyuv to latest.

--- tornado/ioloop.py
+++ tornado/ioloop.py
@@ -30,6 +30,7 @@ from __future__ import absolute_import, division
 
 import datetime
 import errno
+import functools
 import heapq
 import os
 import logging
@@ -622,165 +623,154 @@ class _Select(object):
             events[fd] = events.get(fd, 0) | IOLoop.ERROR
         return events.items()
 
-class PyuvIOLoop(object):
-    NONE = IOLoop.NONE
-    READ = IOLoop.READ
-    WRITE = IOLoop.WRITE
-    ERROR = IOLoop.ERROR
+class UVLoop(IOLoop):
 
-    def __init__(self, impl=None):
-        if impl is not None:
-            raise RuntimeError('When using pyuv the poller implementation cannot be specifiedi')
+    def initialize(self):
         self._loop = pyuv.Loop()
-        self._poll_handles = {}
         self._handlers = {}
         self._callbacks = deque()
         self._timeouts = set()
         self._running = False
         self._stopped = False
+        self._closing = False
         self._thread_ident = None
-
         self._cb_handle = pyuv.Prepare(self._loop)
-
-    @staticmethod
-    def instance():
-        if not hasattr(PyuvIOLoop, "_instance"):
-            if not hasattr(PyuvIOLoop, "_instance"):
-                # New instance after double check
-                PyuvIOLoop._instance = PyuvIOLoop()
-        return PyuvIOLoop._instance
-
-    @staticmethod
-    def initialized():
-        """Returns true if the singleton instance has been created."""
-        return hasattr(PyuvIOLoop, "_instance")
-
-    def install(self):
-        """Installs this IOLoop object as the singleton instance.
-
-        This is normally not necessary as `instance()` will create
-        an IOLoop on demand, but you may want to call `install` to use
-        a custom subclass of IOLoop.
-        """
-        assert not PyuvIOLoop.initialized()
-        PyuvIOLoop._instance = self
-
-    def _close_loop_handles(self):
-        def cb(handle):
-            try:
-                handle.close()
-            except Exception:
-                pass
-        self._loop.walk(cb)
+        self._signal_checker = pyuv.util.SignalChecker(self._loop, self._fdwaker.reader.fileno())
+        self._signal_checker.unref()
 
     def close(self, all_fds=False):
-        if all_fds:
-            self._handlers = {}
-            self._close_loop_handles()
-            # Run the loop so the close callbacks are fired and memory is freed
-            # It will not block because all handles are closed
-            assert not self._loop.run_once(), "there are pending handles"
+        # NOTE: all_fds is disregarded, everything is closed
+        with self._callback_lock:
+            self._closing = True
+        self._fdwaker.close()
+        self._handlers = {}
+        self._close_loop_handles()
+        # Run the loop so the close callbacks are fired and memory is freed
+        assert not self._loop.run(pyuv.UV_RUN_NOWAIT), "there are pending handles"
+        self._loop = None
 
     def add_handler(self, fd, handler, events):
         if fd in self._handlers:
             raise IOError("fd %d already registered" % fd)
         poll = pyuv.Poll(self._loop, fd)
-        poll.fd = fd
-        self._handlers[fd] = (poll, handler)
+        poll.handler = handler
+        self._handlers[fd] = poll
         poll_events = 0
-        if events & PyuvIOLoop.READ:
+        if events & IOLoop.READ:
             poll_events |= pyuv.UV_READABLE
-        if events & PyuvIOLoop.WRITE:
+        if events & IOLoop.WRITE:
             poll_events |= pyuv.UV_WRITABLE
         poll.start(poll_events, self._handle_poll_events)
 
     def update_handler(self, fd, events):
-        poll, _ = self._handlers[fd]
+        poll = self._handlers[fd]
         poll_events = 0
-        if events & PyuvIOLoop.READ:
+        if events & IOLoop.READ:
             poll_events |= pyuv.UV_READABLE
-        if events & PyuvIOLoop.WRITE:
+        if events & IOLoop.WRITE:
             poll_events |= pyuv.UV_WRITABLE
         poll.start(poll_events, self._handle_poll_events)
 
     def remove_handler(self, fd):
-        self._handlers.pop(fd, None)
-
-    def set_blocking_signal_threshold(self, seconds, action):
-        raise NotImplementedError
-
-    def set_blocking_log_threshold(self, seconds):
-        raise NotImplementedError
-
-    def log_stack(self, signal, frame):
-        raise NotImplementedError
+        poll = self._handlers.pop(fd, None)
+        if poll is not None:
+            poll.close()
+            poll.handler = None
 
     def start(self):
+        if not logging.getLogger().handlers:
+            # The IOLoop catches and logs exceptions, so it's
+            # important that log output be visible.  However, python's
+            # default behavior for non-root loggers (prior to python
+            # 3.2) is to print an unhelpful "no handlers could be
+            # found" message rather than the actual log entry, so we
+            # must explicitly configure logging if we've made it this
+            # far without anything.
+            logging.basicConfig()
         if self._stopped:
             self._stopped = False
             return
+        old_current = getattr(IOLoop._current, "instance", None)
+        IOLoop._current.instance = self
         self._running = True
-        self._loop.update_time()
+
+        # pyuv won't interate the loop if the poll is interrupted by
+        # a signal, so make sure we can wake it up to catch signals
+        # registered with the signal module
+        #
+        # If someone has already set a wakeup fd, we don't want to
+        # disturb it.  This is an issue for twisted, which does its
+        # SIGCHILD processing in response to its own wakeup fd being
+        # written to.  As long as the wakeup fd is registered on the IOLoop,
+        # the loop will still wake up and everything should work.
+        old_wakeup_fd = None
+        self._signal_checker.stop()
+        if hasattr(signal, 'set_wakeup_fd') and os.name == 'posix':
+            # requires python 2.6+, unix.  set_wakeup_fd exists but crashes
+            # the python process on windows.
+            try:
+                old_wakeup_fd = signal.set_wakeup_fd(self._fdwaker.writer.fileno())
+                if old_wakeup_fd != -1:
+                    # Already set, restore previous value.  This is a little racy,
+                    # but there's no clean get_wakeup_fd and in real use the
+                    # IOLoop is just started once at the beginning.
+                    signal.set_wakeup_fd(old_wakeup_fd)
+                    old_wakeup_fd = None
+                else:
+                    self._signal_checker.start()
+            except ValueError:  # non-main thread
+                pass
+
         while self._running:
-            # We should use run() here, but we need to have break() for that
-            self._loop.run_once()
+            self._loop.run(pyuv.UV_RUN_ONCE)
             # reset the stopped flag so another start/stop pair can be issued
         self._stopped = False
+        IOLoop._current.instance = old_current
+        if old_wakeup_fd is not None:
+            signal.set_wakeup_fd(old_wakeup_fd)
 
     def stop(self):
         self._running = False
         self._stopped = True
-
-    def running(self):
-        """Returns true if this IOLoop is currently running."""
-        return self._running
+        self._waker.wake()
 
     def add_timeout(self, deadline, callback):
-        timeout = _PyuvTimeout(deadline, callback, io_loop=self)
+        timeout = _Timeout(deadline, callback, io_loop=self)
         self._timeouts.add(timeout)
         return timeout
 
     def remove_timeout(self, timeout):
         self._timeouts.remove(timeout)
         timer = timeout._timer
-        if timer.active:
-            timer.stop()
+        timer.stop()
 
-    def add_callback(self, callback):
+    def add_callback(self, callback, *args, **kwargs):
+        if self._closing:
+            raise RuntimeError("IOLoop is closing")
         was_active = self._cb_handle.active
-        self._callbacks.append(callback)
+        self._callbacks.append(functools.partial(callback, *args, **kwargs))
         if not was_active:
             self._cb_handle.start(self._prepare_cb)
 
-    def handle_callback_exception(self, callback):
-        """This method is called whenever a callback run by the IOLoop
-        throws an exception.
-
-        By default simply logs the exception as an error.  Subclasses
-        may override this method to customize reporting of exceptions.
-
-        The exception itself is not passed explicitly, but is available
-        in sys.exc_info.
-        """
-        logging.error("Exception in callback %r", callback, exc_info=True)
-
-    def _run_callback(self, callback):
-        try:
-            callback()
-        except Exception:
-            self.handle_callback_exception(callback)
+    def add_callback_from_signal(self, callback, *args, **kwargs):
+            self.add_callback(callback, *args, **kwargs)
 
     def _handle_poll_events(self, handle, poll_events, error):
         events = 0
-        if error is None:
+        if error is not None:
+            # Some error was detected, signal readability and writability so that the
+            # handler gets and handles the error
+            events |= IOLoop.READ
+            events |= IOLoop.WRITE
+        else:
             if poll_events & pyuv.UV_READABLE:
-                events |= PyuvIOLoop.READ
+                events |= IOLoop.READ
             if poll_events & pyuv.UV_WRITABLE:
-                events |= PyuvIOLoop.WRITE
-        fd = handle.fd
+                events |= IOLoop.WRITE
+        fd = handle.fileno()
         try:
-            self._handlers[fd][1](fd, events)
-        except (OSError, IOError), e:
+            self._handlers[fd].handler(fd, events)
+        except (OSError, IOError) as e:
             if e.args[0] == errno.EPIPE:
                 # Happens when the client closes the connection
                 pass
@@ -796,23 +786,29 @@ class PyuvIOLoop(object):
         while callbacks:
             self._run_callback(callbacks.popleft())
 
+    def _close_loop_handles(self):
+        def cb(handle):
+            if not handle.closed:
+                handle.close()
+        self._loop.walk(cb)
 
-class _PyuvTimeout(object):
+class _UVTimeout(object):
     """An IOLoop timeout, a UNIX timestamp and a callback"""
 
     # Reduce memory overhead when there are lots of pending callbacks
     __slots__ = ['deadline', 'callback', 'io_loop', '_timer']
 
-    def __init__(self, deadline, callback, io_loop=None):
+    def __init__(self, deadline, callback, io_loop):
+        now = io_loop.time()
         if isinstance(deadline, (int, long, float)):
             self.deadline = deadline
         elif isinstance(deadline, datetime.timedelta):
-            self.deadline = time.time() + _PyuvTimeout.timedelta_to_seconds(deadline)
+            self.deadline = now + _Timeout.timedelta_to_seconds(deadline)
         else:
             raise TypeError("Unsupported deadline %r" % deadline)
         self.callback = callback
-        self.io_loop = io_loop or PyuvIOLoop.instance()
-        timeout = max(self.deadline - time.time(), 0)
+        self.io_loop = io_loop or IOLoop.instance()
+        timeout = max(self.deadline - now, 0)
         self._timer = pyuv.Timer(self.io_loop._loop)
         self._timer.start(self._timer_cb, timeout, 0.0)
 
@@ -829,41 +825,11 @@ class _PyuvTimeout(object):
         return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10 ** 6) / float(10 ** 6)
 
 
-class PyuvPeriodicCallback(object):
-    def __init__(self, callback, callback_time, io_loop=None):
-        self.callback = callback
-        self.callback_time = callback_time / 1000.0
-        self.io_loop = io_loop or PyuvIOLoop.instance()
-        self._timer = pyuv.Timer(self.io_loop._loop)
-        self._running = False
-
-    def _timer_cb(self, timer):
-        try:
-            self.callback()
-        except Exception:
-            logging.error("Error in periodic callback", exc_info=True)
-
-    def start(self):
-        if self._running:
-            return
-        self._running = True
-        self._timer.start(self._timer_cb, self.callback_time, self.callback_time)
-        self._timer.repeat = self.callback_time
-
-    def stop(self):
-        if not self._running:
-            return
-        self._running = False
-        self._timer.stop()
-
-
-
 # Choose a poll implementation. Use epoll if it is available, fall back to
 # select() for non-Linux platforms
 if pyuv:
-    IOLoop = PyuvIOLoop
-    _Timeout = _PyuvTimeout
-    PeriodicCallback = PyuvPeriodicCallback
+    IOLoop = UVLoop
+    _Timeout = _UVTimeout
 elif hasattr(select, "epoll"):
     # Python 2.6+ on Linux
     _poll = select.epoll
